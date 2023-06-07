cb4562e1824cf39ebbfef6d819fbad145d03252d
Yohane <43869960+shiny-yohane@users.noreply.github.com>
2023-06-07 22:05:07 +0900

    compiling

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <GL/glew.h>
+#include <GL/glut.h>
 #include <cstdio>
 
 int main(int argc, char **argv)
 {
-    printf("Hello World!\n");
+    glutInit(&argc, argv);
+    glutCreateWindow("GLEW Test");
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        // Problem: glewInit failed, something is seriously wrong.
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        // Handle the error appropriately.
+        // ...
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
+    // Further code goes here...
+
     return 0;
 }
