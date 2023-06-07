35c123e78fc26f03775ec058102b838adbd07425
Yohane <43869960+shiny-yohane@users.noreply.github.com>
2023-06-07 23:22:54 +0900

    real cool

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <GL/glew.h>
 #include <GLUT/glut.h>
 #include <cstdio>
+#include <cstdlib>
+
+void closeCallback()
+{
+    // Perform any necessary cleanup or application-specific actions
+    // before closing the window
+    // ...
+
+    // Exit the program or close the window
+    // exit(0); // or use glutDestroyWindow(windowId) if you want to close only the current window
+    glutDestroyWindow(glutGetWindow());
+    std::exit(0);
+}
 
 static void Render()
 {
@@ -24,6 +37,8 @@ int main(int argc, char **argv) // why ** ?
 
     int win = glutCreateWindow("GLEW Test");
     printf("window id: %d\n", win);
+
+    glutWMCloseFunc(closeCallback);
 
     GLclampf Red = 1.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f; // GLclampf?
     glClearColor(Red, Green, Blue, Alpha);
