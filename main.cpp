e932e0be47e1decc2cc57861b9af9bb4cf6abb8f
Yohane <43869960+shiny-yohane@users.noreply.github.com>
2023-06-10 16:34:44 +0900

    culling

--- main.cpp
+++ main.cpp
@@ -33,10 +33,19 @@ void createVertexBuffer()
 void render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
 
     GLfloat vertices[] = {-1.0f, -1.0f, 0.0f,
-                          1.0f, -1.0f, 0.0f,
-                          0.0f, 1.0f, 0.0f};
+                          0.0f, 1.0f, 0.0f,
+                          1.0f, -1.0f, 0.0f};
+
+    GLfloat *p = vertices;
+
+    // GLfloat vertices[] = {-1.0f, -1.0f, 0.0f,
+    //                       1.0f, -1.0f, 0.0f,
+    //                       0.0f, 1.0f, 0.0f};
 
     GLfloat colors[] = {1.0f, 0.0f, 0.0f,  // Red color for vertex 1
                         0.0f, 1.0f, 0.0f,  // Green color for vertex 2
